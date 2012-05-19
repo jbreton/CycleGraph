dfdf11b25a01223be74d70146facc46add765b0e
Jonathan <jbreton@kronostechnologies.com>
2012-05-19 17:32:33 -0400

    POC marker showing the place on the map for the current graph point

--- class/CycleGraph/Application.php
+++ class/CycleGraph/Application.php
@@ -80,6 +80,15 @@ class Application {
 				'elevation' => array('name' => 'Elevation', 'data' => array())
 			);
 			
+			$series2 = array(
+				'speed' => array('name' => 'Vitesse', 'data' => array()),
+				'max_speed' => array('name' => 'Vitesse max', 'data' => array()),
+				'avg_speed' => array('name' => 'Vitesse moy', 'data' => array()),
+				'relative_avg_speed' => array('name' => 'Vitesse avg relative', 'data' => array())
+			);	
+			
+			$points_lon_lat = array();
+			$prev_stopped_time = 0;
 			foreach($ride->points as $index => $point) {
 				if($point->stoppedTime != $prev_stopped_time) {
 					$prev_stopped_time = $point->stoppedTime;
@@ -90,166 +99,193 @@ class Application {
 						'color' => '#555555'
 					);
 				}
+				
+				$points_lon_lat[$index] = array('lon' => (float)$point->longitude, 'lat' => (float)$point->latitude);
 
 				$series['cadence']['data'][] = array($index, (int)$point->cadence);
 				$series['hr']['data'][] = array($index, (int)$point->heartRate);
 				$series['avg_hr']['data'][] = array($index, (int)$ride->avgHeartRate);
 				$series['relative_avg_hr']['data'][] = array($index, (int)$point->avgHeartRate);
 				$series['elevation']['data'][] = array($index, (int)$point->elevation);
-			}
-			
-			echo '<script type="text/javascript">
-$(function () {
-    var chart;
-    $(document).ready(function() {
-        chart = new Highcharts.Chart({
-            chart: {
-                renderTo: \'container\',
-                type: \'spline\',
-                marginRight: 130,
-                marginBottom: 25
-            },
-            title: {
-                text: \''.$ride->name.'\',
-                x: -20 //center
-            },
-            xAxis: {
-				type:\'linear\',
-				plotBands: '.json_encode($bands).',
-            },
-            yAxis: {
-                title: {
-                    text: \'Speed/HR\'
-                },
-                plotLines: [{
-                    value: 0,
-                    width: 1,
-                    color: \'#808080\'
-                }],
-				min : 0
-            },
-            tooltip: {
-                formatter: function() {
-                        return \'<b>\'+ this.series.name +\'</b><br/>\'+
-                        this.x +\': \'+ this.y;
-                }
-            },
-            legend: {
-                layout: \'vertical\',
-                align: \'right\',
-                verticalAlign: \'top\',
-                x: -10,
-                y: 100,
-                borderWidth: 0
-            },
-            series: '.json_encode(array_values($series)).',
-			plotOptions: {
-				series: {
-					marker: {
-						enabled: false,
-						states: {
-							hover: {
-								enabled: true
-							}
-						}
-					}
-				}
-			}
-        });
-    });
-    
-});
-		</script>
-		<div id="container" style="min-width: 400px; height: 400px; margin: 0 auto"></div>';
-		
-			$series2 = array(
-				'speed' => array('name' => 'Vitesse', 'data' => array()),
-				'max_speed' => array('name' => 'Vitesse max', 'data' => array()),
-				'avg_speed' => array('name' => 'Vitesse moy', 'data' => array()),
-				'relative_avg_speed' => array('name' => 'Vitesse avg relative', 'data' => array())
-			);	
-			
-			$prev_stopped_time = 0;
-			foreach($ride->points as $index => $point) {
+
 				$series2['speed']['data'][] = array($index, (float)$point->speed);
 				$series2['max_speed']['data'][] = array($index, (float)$ride->maxSpeed);
 				$series2['avg_speed']['data'][] = array($index, (float)$ride->avgSpeed);
-				$series2['relative_avg_speed']['data'][] = array($index, (float)$point->avgSpeed);
+				$series2['relative_avg_dspeed']['data'][] = array($index, (float)$point->avgSpeed);
 			}
+			
+			echo '<div id="ride_map" style="height: 640px; width: 980px; margin: 0 auto;"></div>
+				<div id="container" style="width: 980px; height: 400px; margin: 0 auto"></div>
+				<div id="container2" style="width: 980px; height: 400px; margin: 0 auto"></div>
+				<script>
+			$(function() {
+				 var chart;
+				 var chart2;
+				 var map;
+				 
+				 var marker;
+				 
+				 var points_lon_lat = '.json_encode($points_lon_lat).';
 
-			echo '<script type="text/javascript">
-	$(function () {
-		var chart2;
-		$(document).ready(function() {
-			console.debug($(\'.paste_gpx\'));
-			$(\'#paste_gpx\').button().click(function() {
-				console.debug(\'click\');
-			});
+				$(document).ready(function() {
+					var myOptions = {
+						zoom: 12,
+						mapTypeId: google.maps.MapTypeId.ROADMAP
+					};
+					map = new google.maps.Map(document.getElementById(\'ride_map\'),myOptions); 
+					if(navigator.geolocation) {
+					  navigator.geolocation.getCurrentPosition(function(position) {
+						var pos = new google.maps.LatLng(position.coords.latitude,
+														 position.coords.longitude);
 
+						console.debug(position.coords.latitude);
+						console.debug(position.coords.longitude);
 
-			chart2 = new Highcharts.Chart({
-				chart: {
-					renderTo: \'container2\',
-					type: \'spline\',
-					marginRight: 130,
-					marginBottom: 25
-				},
-				title: {
-					text: \''.$ride->name.'\',
-					x: -20 //center
-				},
-				xAxis: {
-					type:\'linear\',
-					plotBands: '.json_encode($bands).',
-				},
-				yAxis: {
-					title: {
-						text: \'Speed\'
-					},
-					plotLines: [{
-						value: 0,
-						width: 1,
-						color: \'#808080\'
-					}],
-					min : 0,
-					max : '.(ceil($ride->maxSpeed / 10) * 10).'
-				},
-				tooltip: {
-					formatter: function() {
-							return \'<b>\'+ this.series.name +\'</b><br/>\'+
-							this.x +\': \'+ this.y;
+						map.setCenter(pos);
+						
+						var marker_options = {
+							clickable:false,
+							draggable:false,
+							map:map,
+							position:pos
+						};
+
+						marker = new google.maps.Marker(marker_options);
+					  });
 					}
-				},
-				legend: {
-					layout: \'vertical\',
-					align: \'right\',
-					verticalAlign: \'top\',
-					x: -10,
-					y: 100,
-					borderWidth: 0
-				},
-				series: '.json_encode(array_values($series2)).',
-				plotOptions: {
-				series: {
-					marker: {
-						enabled: false,
-						states: {
-							hover: {
-								enabled: true
+				
+					chart = new Highcharts.Chart({
+						chart: {
+							renderTo: \'container\',
+							type: \'spline\',
+							marginRight: 130,
+							marginBottom: 25
+						},
+						tooltip: {
+							formatter: function() {
+							    var s = \'<b>\'+ this.x +\'</b>\';
+
+								$.each(this.points, function(i, point) {
+									s += \'<br/>\'+ point.series.name +\': \'+ point.y +\'m\';
+								});
+								
+								console.debug(points_lon_lat[this.x]);
+								
+								var p = new google.maps.LatLng(points_lon_lat[this.x].lat, points_lon_lat[this.x].lon);
+								console.debug(p);
+								marker.setPosition(p);
+
+								
+									map.panTo(p);	
+									
+
+								return s;
+							},
+							shared: true
+						},
+						title: {
+							text: \'' . $ride->name . '\',
+							x: -20 //center
+						},
+						xAxis: {
+							type:\'linear\',
+							plotBands: ' . json_encode($bands) . ',
+						},
+						yAxis: {
+							title: {
+								text: \'Speed/HR\'
+							},
+							plotLines: [{
+								value: 0,
+								width: 1,
+								color: \'#808080\'
+							}],
+							min : 0
+						},
+						legend: {
+							layout: \'vertical\',
+							align: \'right\',
+							verticalAlign: \'top\',
+							x: -10,
+							y: 100,
+							borderWidth: 0
+						},
+						series: ' . json_encode(array_values($series)) . ',
+						plotOptions: {
+							series: {
+								marker: {
+									enabled: false,
+									states: {
+										hover: {
+											enabled: true
+										}
+									}
+								}
 							}
 						}
-					}
-				}
-			},
-			});
-		});
+					});
 
-	});
+					chart2 = new Highcharts.Chart({
+						chart: {
+							renderTo: \'container2\',
+							type: \'spline\',
+							marginRight: 130,
+							marginBottom: 25
+						},
+						title: {
+							text: \'' . $ride->name . '\',
+							x: -20 //center
+						},
+						xAxis: {
+							type:\'linear\',
+							plotBands: ' . json_encode($bands) . ',
+						},
+						yAxis: {
+							title: {
+								text: \'Speed\'
+							},
+							plotLines: [{
+								value: 0,
+								width: 1,
+								color: \'#808080\'
+							}],
+							min : 0,
+							max : ' . (ceil($ride->maxSpeed / 10) * 10) . '
+						},
+						tooltip: {
+							formatter: function() {
+									return \'<b>\'+ this.series.name +\'</b><br/>\'+
+									this.x +\': \'+ this.y;
+							}
+						},
+						legend: {
+							layout: \'vertical\',
+							align: \'right\',
+							verticalAlign: \'top\',
+							x: -10,
+							y: 100,
+							borderWidth: 0
+						},
+						series: ' . json_encode(array_values($series2)) . ',
+						plotOptions: {
+						series: {
+							marker: {
+								enabled: false,
+								states: {
+									hover: {
+										enabled: true
+									}
+								}
+							}
+						}
+					},
+					});
+				});
+			});
 		</script>
-		<div id="container2" style="min-width: 400px; height: 400px; margin: 0 auto"></div>';
+		';
 			
-		echo '
-		<input type="button" id="paste_gpx" value="Paste a GPS file" />';
+		
 			
 			$this->showFooter();
 		}
@@ -266,6 +302,7 @@ $(function () {
 		<link rel="stylesheet" href="css/jquery.ui.all.css" TYPE="text/css" media="all">
 		<script src="js/highcharts.js"></script>
 		<script src="js/gray.js"></script>
+		<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?sensor=true"></script>
 	</head>
 	<body>';
 	}
